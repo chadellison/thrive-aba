767d0383f11ef9d405842af6289af7ec794918d2
Chad Ellison <chad.ellison0123@gmail.com>
2018-12-16 22:41:46 -0600

    react-native spike

---
+++ components/Message.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+class Message extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Hello Home
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default Message;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import Message from '../components/Message'
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
@@ -33,34 +34,10 @@ export default class HomeScreen extends React.Component {
           </View>
 
           <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-              But is this the thing?
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
+            <Message/>
           </View>
         </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
       </View>
     );
   }
